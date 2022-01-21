96eff13d95079ccbdd479324d145259075f62fff
assafvan <vanunuassaf@gmail.com>
2022-01-21 16:02:54 +0200

    change pass

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://assafv:silvias14@cluster0.sta9t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  "mongodb+srv://assafv:kJuMWdyUWbBJlKea@cluster0.sta9t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 );
