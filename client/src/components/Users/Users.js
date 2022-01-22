907a08b5036391778e3dccd4cfe87d18ceb22961
assafvan <vanunuassaf@gmail.com>
2022-01-23 01:28:11 +0200

    trying to add allusers

--- client/src/components/Users/Users.js
+++ client/src/components/Users/Users.js
@@ -20,20 +20,20 @@ export default function Users() {
     return () => {};
   }, []);
 
-  // const showUsers = () => {
-  //   userData.map(({ _id, id, cash, credit }) => {
-  //     return (
-  //       <Fragment key={_id}>
-  //         <tr>
-  //           <td>{id}</td>
-  //           <td>{cash}</td>
-  //           <td>{credit}</td>
-  //         </tr>
-  //       </Fragment>
-  //     );
-  //   });
-  // };
+  const showUsers = () => {
+    userData.map(({ _id, id, cash, credit }) => {
+      return (
+        <Fragment key={_id}>
+          <tr>
+            <td>{id}</td>
+            <td>{cash}</td>
+            <td>{credit}</td>
+          </tr>
+        </Fragment>
+      );
+    });
+  };
 
-  return <div>users</div>;
-  // return <div>{showUsers}</div>;
+  // return <div>users</div>;
+  return <div>{showUsers}</div>;
 }
